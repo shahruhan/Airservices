f51a84b0c2b7d0f0966f54c854c8727792640800
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-01-07 21:11:32 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -26,19 +26,6 @@ app.use(express.static(path.join(__dirname, './client/build')));
    res.sendFile(path.join(__dirname, './client/build/index.html'));
  });
 
- // app.get('*', (req, res) => {
- //      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
- //    });
-
-// if( process.env.NODE_ENV == "production"){
-//     app.use(express.static(path.join(__dirname, 'client/build')));
-
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-
-// }
-
 // ************ Link the router file ************
 app.use(require('./router/auth'));
 
