b055ba3134a05d83acf6fc71b3557911eaba6ac2
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-01-08 11:31:23 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const dotenv = require('dotenv');
+const express = require("express");
 const app = express();
 dotenv.config({path: './config.env'});
 require('./db/conn');
